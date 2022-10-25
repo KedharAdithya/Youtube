f3c5317175cc0c916874659786cb98b0008fa665
KedharAdithya <kedhar.kambhampati@gmail.com>
2022-10-25 14:37:03 +0530

    First commit

---
+++ src/store/reducers/getHomePageVideos.ts
@@ -0,0 +1,27 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { RootState } from "..";
+import { HomePageVideos } from "../../Types";
+import { parseData } from "../../utils";
+import { YOUTUBE_API_URL } from "../../utils/constants";
+
+const API_KEY ="AIzaSyAQc0MyLcUXHNxZ5PqwioHoipxjQPGT2P8";
+
+export const getHomePageVideos = createAsyncThunk(
+  "youtubeApp/homePageVidoes",
+  async (isNext: boolean, { getState }) => {
+    const {
+      youtubeApp: { nextPageToken: nextPageTokenFromState, videos },
+    } = getState() as RootState;
+    const {
+      data: { items, nextPageToken },
+    } = await axios.get(
+      `${YOUTUBE_API_URL}/search?maxResults=20&q="English Songs"&key=${API_KEY}&part=snippet&type=video&${
+        isNext ? `pageToken=${nextPageTokenFromState}` : ""
+      }`
+    );
+    console.log({ items, nextPageTokenFromState, nextPageToken });
+    const parsedData: HomePageVideos[] = await parseData(items);
+    return { parsedData: [...videos, ...parsedData], nextPageToken };
+  }
+);
